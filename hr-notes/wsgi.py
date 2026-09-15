b9c23cc6025a62c7bc00e174b66e065fa9bbbf47
mailo <mailo@shun.kin>
2026-09-15 19:03:52 +0300

    chore: вынести hr-notes (TeamBook) в отдельный репозиторий shoonia69/teambook

--- hr-notes/wsgi.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Точка входа для gunicorn: инициализирует БД и отдаёт приложение."""
-from app import app, init_db
-
-init_db()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(__import__("os").environ.get("PORT", 5000)))
